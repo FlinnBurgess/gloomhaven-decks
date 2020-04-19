b1f9702d8ec1c7a4da5104eaaae208a87840e281
Flinn Burgess <flinn.burgess@gmail.com>
2020-04-19 22:44:02 +0100

    Add infusion modifier cards

---
+++ lib/src/cards/infusion_card.dart
@@ -0,0 +1,12 @@
+import 'package:gloomhaven_decks/src/attack_modifier_result.dart';
+import 'package:gloomhaven_decks/src/cards/attack_modifier_card.dart';
+import 'package:gloomhaven_decks/src/elemental_infusions.dart';
+
+class InfusionCard extends AttackModifierCard {
+  InfusionCard(Infusion infusion, bool isRolling)
+      : super(infusionEffect(infusion), isRolling);
+}
+
+Function(AttackModifierResult) infusionEffect(Infusion infusion) {
+  return (AttackModifierResult result) => result.addInfusion(infusion);
+}
