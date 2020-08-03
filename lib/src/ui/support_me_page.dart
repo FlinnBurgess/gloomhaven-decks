c6f29bfe82f7acdcbc982e3b3d00b398a5575d62
Flinn Burgess <flinn.burgess@gmail.com>
2020-08-03 15:27:40 +0100

    Add email address to support me page

--- lib/src/ui/support_me_page.dart
+++ lib/src/ui/support_me_page.dart
@@ -19,37 +19,40 @@ class SupportMePage extends StatelessWidget {
                 child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Transform.scale(
-                scale: 1.1,
-                child: Wrap(
-                  direction: Axis.vertical,
-                  spacing: 25,
-                  children: <Widget>[
-                    Container(
-                        width: 300,
-                        child: OutlinedText.blackAndWhite(
-                            'If you are enjoying the app, here are some ways you can support me',
-                            TextAlign.center)),
-                    Container(
-                        width: 300,
-                        child: Center(
-                            child: InkWell(
-                          child: OutlinedText(
-                              'Buy me a coffee', Colors.blue, Colors.black),
-                          onTap: () => launch(
-                              'https://www.buymeacoffee.com/flinnburgess'),
-                        ))),
-                    Container(
-                        width: 300,
-                        child: Center(
-                            child: InkWell(
-                          child: OutlinedText(
-                              'Rate the app', Colors.blue, Colors.black),
-                          onTap: () => launch(
-                              'https://play.google.com/store/apps/details?id=com.flinnburgess.gloomhaven_deck_tracker'),
-                        ))),
-                  ],
-                )),
+            Wrap(
+              direction: Axis.vertical,
+              spacing: 25,
+              children: <Widget>[
+                Container(
+                    width: 300,
+                    child: OutlinedText.blackAndWhite(
+                        'Hi! Thanks for using Gloomhaven Deck Tracker. If you are enjoying the app, here are some ways you can support me',
+                        TextAlign.center)),
+                Container(
+                    width: 300,
+                    child: Center(
+                        child: InkWell(
+                      child: OutlinedText(
+                          'Buy me a coffee', Colors.blue, Colors.black),
+                      onTap: () =>
+                          launch('https://www.buymeacoffee.com/flinnburgess'),
+                    ))),
+                Container(
+                    width: 300,
+                    child: Center(
+                        child: InkWell(
+                      child: OutlinedText(
+                          'Rate the app', Colors.blue, Colors.black),
+                      onTap: () => launch(
+                          'https://play.google.com/store/apps/details?id=com.flinnburgess.gloomhaven_deck_tracker'),
+                    ))),
+                Container(
+                    width: 300,
+                    child: OutlinedText.blackAndWhite(
+                        'If you spot any bugs or have suggestions for how I can improve the app, feel free to contact me at flinn@thetimelydeveloper.com',
+                        TextAlign.center)),
+              ],
+            ),
           ],
         ))));
   }
